aee8ed73fd26d75d125e10e3ee9c436343db1c2e
balamosh <provasmif@mail.ru>
2021-08-17 12:38:56 -0400

    02_06 course project ORGANIZED

--- 02_yellow_belt/course_project/make_tar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm cource_project_yellow.tar
-tar -cvf cource_project_yellow.tar srcs/* includes/*
--- 02_yellow_belt/course_project/test.sh
+++ 02_yellow_belt/course_project/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-diff <(cat 01_test | ./project.out) 01_out
-diff <(cat 02_test | ./project.out) 02_out
-diff <(cat 03_test | ./project.out) 03_out
-diff <(cat 04_test | ./project.out) 04_out
-diff <(cat 05_test | ./project.out) 05_out
+diff <(cat tests/01_test | ./project.out) tests/01_out
+diff <(cat tests/02_test | ./project.out) tests/02_out
+diff <(cat tests/03_test | ./project.out) tests/03_out
+diff <(cat tests/04_test | ./project.out) tests/04_out
+diff <(cat tests/05_test | ./project.out) tests/05_out
